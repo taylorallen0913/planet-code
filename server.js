b805328af237f694f8df733d77cef948366d1229
Taylor Allen <taylorallen0913@gmail.com>
2020-06-05 00:36:47 -0700

    build for production

--- server.js
+++ server.js
@@ -41,9 +41,9 @@ app.use('/api/judge', judge);
 
 // For production
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname + '/client/build/index.html'));
-// });
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 
