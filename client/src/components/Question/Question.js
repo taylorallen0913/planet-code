9efaf096a20bb21efcd3f563051345e1d0a84b17
Taylor Allen <taylorallen0913@gmail.com>
2020-06-05 00:26:09 -0700

    removed to reset file names

--- client/src/components/Question/Question.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Question = (props) => {
-    return <h1>Question</h1>;
-};
-
-export default Question;
--- client/src/pages/noRouteMatch/index.js
+++
@@ -1,3 +0,0 @@
-import NoRouteMatch from './NoRouteMatch';
-
-export default NoRouteMatch;
